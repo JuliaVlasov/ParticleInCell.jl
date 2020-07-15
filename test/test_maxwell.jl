0bdde453e47fa16ecee5d820f5106cb9ecefd17f
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-07-15 12:53:53 +0200

    test maxwell OK

--- test/test_maxwell.jl
+++ test/test_maxwell.jl
@@ -1,57 +1,65 @@
 using ParticleInCell 
-using OffsetArrays
+using LinearAlgebra
 
 
 @testset "Maxwell solver" begin
 
 
 dimx, dimy = 1, 1
-nx, ny = 64, 64
+nx, ny = 100, 100
 e0 = 1
 c = 1
-md, nd = 2, 2  # number of modes along each direction
 dt = 0.001
-nstep = 1 ÷ dt
+nstep = 10
 
 mesh = Mesh( dimx, nx, dimy, ny )
 
 maxwell = MaxwellSolver( mesh, c, e0) 
 
-omega = c * sqrt((md*pi/dimx)^2+(nd*pi/dimy)^2)
+omega = c * sqrt((2π/dimx)^2+(2π/dimy)^2)
 
-ex = OffsetArray(zeros(nx+1,ny+1), 0:nx,0:ny)
-ey = OffsetArray(zeros(nx+1,ny+1), 0:nx,0:ny)
-bz = OffsetArray(zeros(nx+1,ny+1), 0:nx,0:ny)
+ex = zeros(nx+1,ny+1)
+ey = zeros(nx+1,ny+1)
+bz = zeros(nx+1,ny+1)
 
 # Ex and Ey are set at t = 0.0
 # Bz is set at  t = -dt/2
 
-for j=0:ny, i=0:nx
-    bz[i,j] = - cos(md*pi*mesh.x[i]) * cos(nd*pi*mesh.y[j]) * cos(omega*(-0.5*dt))
-end  
+xn = mesh.x
+yn = transpose(mesh.y)
 
-time = 0
+xc = 0.5 .* ( xn[1:end-1] .+ xn[2:end])
+yc = transpose(0.5 .* ( yn[1:end-1] .+ yn[2:end]))
+
+@show size(xc)
+@show size(yc)
+
+@. maxwell.ex = 0
+@. maxwell.ey = 0
+@. maxwell.bz = - cos(2π*xc) * cos(2π*yc) * cos(omega*(-0.5*dt))
+
+t = 0
+
+sol_bz = - cos.(2π*xn) .* cos.(2π*yn)
+sol_ex = + cos.(2π*xn) .* sin.(2π*yn) .* 2π / omega
+sol_ey = - sin.(2π*xn) .* cos.(2π*yn) .* 2π / omega
     
 for istep = 1:nstep # Loop over time
     
     faraday!(maxwell, bz, dt)   
 
-    time = time + 0.5dt
-    
+    t = t + 0.5dt
+
+    @test maximum(abs.(bz .- sol_bz .* cos(omega*t))) < 0.001
+
     ampere_maxwell!(maxwell, ex, ey, dt) 
 
-    time = time + 0.5dt
+    t = t + 0.5dt
+
+    @test maximum(abs.(ex .- sol_ex .* sin(omega*t))) < 0.001
+    @test maximum(abs.(ey .- sol_ey .* sin(omega*t))) < 0.001
+
     
 end # next time step
     
-err_l2 = 0.0
-time = (nstep-0.5)*dt
-for j = 0:ny, i = 0:nx
-    th_bz = (- cos(md*pi*mesh.x[i]) * cos(nd*pi*mesh.y[j]) * cos(omega*time))
-    err_l2 += (bz[i,j] - th_bz)^2
-end
-
-
-@test err_l2 < 0.001
-
 end
