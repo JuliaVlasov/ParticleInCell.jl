d734051a041fa6af4f32865304a5f64e00297619
Pierre Navaro <navaro@math.cnrs.fr>
2020-10-10 15:26:36 +0200

    Update TwoStreamInstability.jl

--- examples/TwoStreamInstability.jl
+++ examples/TwoStreamInstability.jl
@@ -2,98 +2,130 @@
 # ---
 # jupyter:
 #   jupytext:
+#     formats: ipynb,jl:light
 #     text_representation:
 #       extension: .jl
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.6.0
 #   kernelspec:
-#     display_name: Julia 1.0.1
+#     display_name: Julia 1.5.1
 #     language: julia
-#     name: julia-1.0
+#     name: julia-1.5
 # ---
 
-using Plots
 using LinearAlgebra
-using SparseArrays
+using Plots
+using ProgressMeter
 using Random
+using SparseArrays
 
 # +
-L  = 20π       # Domain size
-DT = 0.005     # Time step
-NT = 10000     # Number of time steps
-NG = 320       # Number of grid cells
-N  = NG * 20   # Number of particles
-WP = 1.        # Plasma frequency
-QM = -1.       # Charge/mass ratio
-V0 = 0.9       # Stream velocity
-VT = 0.0000001 # Thermal speed
+const L  = 20π       # Domain size
+const DT = 0.005     # Time step
+const NT = 10000     # Number of time steps
+const NG = 320       # Number of grid cells
+const N  = NG * 20   # Number of particles
+const WP = 1.        # Plasma frequency
+const QM = -1.       # Charge/mass ratio
+const V0 = 0.9       # Stream velocity
+const VT = 0.0000001 # Thermal speed
 
 # perturbation 
-XP1 = 1.0 
-mode = 1
+const XP1 = 1.0 
+const mode = 1
 
-Q = WP^2 / (QM*N/L)  # rho0*L/N: charge carried by a single particle?
-rho_back = -Q*N/L  # Background charge density?
-dx = L / NG # Grid step
-
+const Q = WP^2 / (QM*N/L)  # rho0*L/N: charge carried by a single particle?
+const rho_back = -Q*N/L  # Background charge density?
+const dx = L / NG # Grid step
+    
 # Auxilliary vectors
-p = [1:N; 1:N]  # Some indices up to N
-Poisson = spdiagm(-1 => ones(Float64,NG-2),
-                   0 => -2*ones(Float64,NG),
-                   1 => ones(Float64,NG-2))
+const p = [1:N; 1:N]  # Some indices up to N
 
-# +
-# Cell center coordinates
-xg = collect(range(0, stop=L-dx, length=NG)) .+ dx/2
-xp = collect(range(0, stop=L-L/N, length=N))  # Particle positions
-
-rng = MersenneTwister(1234)
-
-vp = VT .* (1 .- VT.^2).^(-0.5) .* randn(rng, Float64, N) # Particle momentum, initially Maxwellian
-pm = collect(0:N-1)
-pm = 1 .- 2 * mod.(pm.+1, 2)
-vp .+= pm * (V0 * (1 - V0^2)^(-0.5)) # Momentum + stream velocity
-
-# Add electron perturbation to excite the desired mode
-xp .+= XP1 * (L/N) * sin.(2 * π * xp / L * mode)
-xp[map(x -> x <  0, xp)] .+= L
-xp[map(x -> x >= L, xp)] .-= L
 # -
 
-Poisson = spdiagm(-1 => ones(Float64,NG-2),
-                   0 => -2*ones(Float64,NG),
-                   1 => ones(Float64,NG-2))
-
-function compute_rho(xp)
+function compute_rho!(rho, xp)
     # Project particles -> grid
     g1 = floor.(Int64,floor.(xp/dx .- 0.5))
     g = floor.(Int64,vcat(g1, g1.+1))
     fraz1 = 1 .- abs.(xp/dx .- g1 .- 0.5)
     fraz = vcat(fraz1, 1 .- fraz1)
-    g[map( x -> x < 0   , g)] .+= NG
-    g[map( x -> x > NG-1, g)] .-= NG
+    for i in eachindex
+    g[g .< 0   ] .+= NG
+    g[g .> NG-1] .-= NG
     g .+= 1
-    println(minimum(p)," - ", maximum(p))
-    println(minimum(g)," - ", maximum(g))
     mat = sparse(p, g, fraz, N, NG)
-    Q / dx * vec(sum(mat,dims=1)) .+ rho_back
+    rho .= Q / dx * vec(sum(mat,dims=1)) .+ rho_back
+    return mat
 end
 
-function solve_poisson( rho )
+function solve!( Eg, rho, Poisson )
     # Compute electric field potential
-    Φ = zeros(Float64,NG)
     rho .*= (-dx^2)
-    sol = Poisson \  rho
-    Φ[1:end-1] .= sol[1:end-1]
-    Φ[end] = 0.0
+    rho .= Poisson \ rho
+    Eg[1:end-1] .= rho[1:end-1]
+    Eg[end] = 0.0
     # Electric field on the grid
-    (circshift(Φ, 1) - circshift(Φ, -1)) ./ (2dx)
+    Eg .= (circshift(Eg, 1) .- circshift(Eg, -1)) ./ (2dx)
 end  
 
-rho = compute_rho(xp)  
-@show typeof(rho)
-Eg = solve_poisson(rho)
-plot(xg, Eg)
+function update_position!(xp, vp, DT)
+    # update particle position xp
+    for i in eachindex(xp)
+        xp[i] += vp[i] * DT
+        xp[i] = mod( xp[i], L) # Periodic boundary condition 
+    end
+end
 
-# ![](Eg.png)
+function main()
+    
+    # Cell center coordinates
+    xg = collect(LinRange(0, L, NG+1))[1:end-1] .+ dx/2
+    xp = collect(LinRange(0, L, N+1))[1:end-1]  # Particle positions
+    
+    rng = MersenneTwister(1234)
+    
+    vp = VT .* (1 .- VT.^2).^(-0.5) .* randn(rng, Float64, N) # Particle momentum, initially Maxwellian
+    pm = collect(0:N-1)
+    pm = 1 .- 2 * mod.(pm.+1, 2)
+    vp .+= pm * (V0 * (1 - V0^2)^(-0.5)) # Momentum + stream velocity
+    
+    # Add electron perturbation to excite the desired mode
+    xp .+= XP1 * (L/N) * sin.(2 * π * xp / L * mode)
+    xp[xp .< 0] .+= L #periodic boundary condition
+    xp[xp .> L] .-= L
+    
+    Poisson = spdiagm(-1 => ones(Float64,NG-2),
+                      0 => -2*ones(Float64,NG),
+                      1 => ones(Float64,NG-2));
+    
+    b = Progress(NT+1)
+    PotE = Float64[]
+    Eg = zeros(Float64,NG)
+    rho = zeros(Float64,NG)
+    
+    for it in 1:NT+1
+        update_position!(xp, vp, DT) 
+        mat = compute_rho!(rho, xp)
+        solve!(Eg, rho, Poisson)
+        vp .+= mat * QM * Eg * DT
+        push!(PotE, 0.5 * sum(Eg.^2) * dx) 
+        plt = plot()
+        xlims!(0, L)
+        ylims!(-7, 7)
+        #scatter!(plt, xp[1:2:end], vp[1:2:end], marker = (:circle, 2, 1., :blue, stroke(1, 0.2, :blue, :dot)))
+        #scatter!(plt, xp[2:2:end], vp[2:2:end], marker = (:circle, 2, 1., :red, stroke(1, 0.2, :red, :dot)))
+        next!(b)
+    end #every 50
+    PotE
+end
+
+@time results = main();
+
+# +
+t = (0:NT) .* DT
+
+plot( t, results, yaxis=:log)
+# -
+
+
