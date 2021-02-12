130614244416b4dace36a007d2f3051437cb1f97
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-02-12 21:23:22 +0100

    Update ParticleInCell.jl

--- src/ParticleInCell.jl
+++ src/ParticleInCell.jl
@@ -10,10 +10,6 @@ include("particle_1d1v.jl")
 include("simulation.jl")
 include("particles.jl")
 include("mesh.jl")
-include("maxwell.jl")
 include("fdtd.jl")
-include("interpolate_eb.jl")
-include("compute_current.jl")
-include("compute_rho.jl")
 
 end
