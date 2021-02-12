4b2198de53d60954da20fa87f4f289050eccf28c
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-02-12 21:45:03 +0100

    Update ParticleInCell.jl

--- src/ParticleInCell.jl
+++ src/ParticleInCell.jl
@@ -7,9 +7,9 @@ using OffsetArrays
 import Sobol
 
 include("particle_1d1v.jl")
-include("simulation.jl")
 include("particles.jl")
 include("mesh.jl")
+include("compute_rho.jl")
 include("fdtd.jl")
 
 end
