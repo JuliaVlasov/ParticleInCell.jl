3c496a1313612da317b5e22c32f4d93f5263173a
Pierre Navaro <navaro@math.cnrs.fr>
2021-02-15 22:44:34 +0100

    Update ParticleInCell.jl

--- src/ParticleInCell.jl
+++ src/ParticleInCell.jl
@@ -5,9 +5,9 @@ using Random
 using SparseArrays
 import Sobol
 
+include("mesh.jl")
 include("particle_1d1v.jl")
 include("particles.jl")
-include("mesh.jl")
 include("compute_rho.jl")
 include("fdtd.jl")
 include("pic.jl")
