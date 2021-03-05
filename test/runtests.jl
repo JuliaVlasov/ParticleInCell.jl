dee89be6bfd3c510b82beffcddf5df8dace4a88c
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-03-05 20:24:38 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using LinearAlgebra
 using ParticleInCell
 using Test
 
+include("test_deposition.jl")
 include("test_particle_group.jl")
 include("test_splitting.jl")
 include("test_poisson_2d.jl")
@@ -9,8 +10,7 @@ include("test_particle_mesh_coupling_spline_2d.jl")
 include("test_spline_pp.jl")
 include("test_particle_mesh_coupling_spline_1d.jl")
 include("test_maxwell_1d_fem.jl")
-include("test_deposition.jl")
-include("landau_damping.jl")
+include("test_landau_damping.jl")
 include("test_interpolation.jl")
 include("test_push.jl")
 include("test_fdtd.jl")
