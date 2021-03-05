f1473e5f02b7350794b6e358a0fa466cfcbd75a6
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-03-05 20:24:11 +0100

    Update pic.jl

--- src/pic.jl
+++ src/pic.jl
@@ -15,40 +15,7 @@ open(joinpath(@__DIR__, "pic.f90")) do f90file
     end
 end
 
-function interpolation!(p::Array{Float64,2}, m::TwoDGrid)
 
-    nx = Int32(m.nx)
-    ny = Int32(m.ny)
-    dx = m.dx
-    dy = m.dy
-    nbpart = Int32(size(p)[2])
-
-    ccall(
-        (:interpolation, piclib),
-        Cvoid,
-        (
-            Ref{Int32},
-            Ref{Int32},
-            Ref{Int32},
-            Ref{Float64},
-            Ref{Float64},
-            Ptr{Float64},
-            Ptr{Float64},
-            Ptr{Float64},
-            Ptr{Float64},
-        ),
-        nbpart,
-        nx,
-        ny,
-        dx,
-        dy,
-        m.ex,
-        m.ey,
-        m.bz,
-        p,
-    )
-
-end
 
 function compute_current!(m::TwoDGrid, p::Array{Float64,2})
 
@@ -113,11 +80,22 @@ end
 
 export push_v!
 
-function push_v!(p::Array{Float64,2}, mesh::TwoDGrid, dt::Float64)
+function push_v!(p::Array{Float64,2}, m::TwoDGrid, dt::Float64)
 
     nbpart = Int32(size(p)[2])
+    nx = Int32(m.nx)
+    ny = Int32(m.ny)
+    dx = m.dx
+    dy = m.dy
 
-    ccall((:push_v, piclib), Cvoid, (Ref{Int32}, Ptr{Float64}, Ref{Float64}), nbpart, p, dt)
+    ccall((:push_v, piclib), Cvoid, (Ref{Int32}, Ptr{Float64}, Ref{Float64},
+            Ref{Int32},
+            Ref{Int32},
+            Ref{Float64},
+            Ref{Float64},
+            Ptr{Float64},
+            Ptr{Float64},
+            Ptr{Float64}), nbpart, p, dt, nx, ny, dx, dy, m.ex, m.ey, m.bz)
 
 end
 
